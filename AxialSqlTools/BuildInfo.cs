74444a9129c3e44973f563513530d3715eb7b45c
liangguopeng <554425886@qq.com>
2026-09-09 17:24:29 +0800

    chore(version): 将扩展版本号从 4.12 回退至 1.0.0

--- AxialSqlTools/BuildInfo.cs
+++ AxialSqlTools/BuildInfo.cs
@@ -10,7 +10,7 @@ namespace AxialSqlTools
     /// </summary>
     public static class BuildInfo
     {
-        public const string BuildTime = "2026-09-09 17:09:11";
+        public const string BuildTime = "2026-09-09 17:23:35";
 
         public static string AssemblyVersion
         {
